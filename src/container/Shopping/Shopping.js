682aa262e0b9ab14ea56880efc60b6249159bacc
Marouf <maroufibrahimi7264@gmail.com>
2023-02-28 16:41:26 -0800

    added Shopping component here

--- src/container/Shopping/Shopping.js
+++ src/container/Shopping/Shopping.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import Controls from "../../components/Controls/Controls";
+
 class Shopping extends Component {
   render() {
-    return <div>Shopping</div>;
+    return (
+      <div>
+        <Controls />
+      </div>
+    );
   }
 }
 
