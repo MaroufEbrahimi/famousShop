5c35f06845ea7f35ca38397527fcf0b50dc4f9f2
Marouf <maroufibrahimi7264@gmail.com>
2023-02-28 16:13:27 -0800

    added Shopping component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Template</h1>
-    </div>
-  );
-};
+import Shopping from "./container/Shopping/Shopping";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Shopping />
+      </div>
+    );
+  }
+}
 
 export default App;
