af1408704f79028636674aa5a3defcbef3bc9543
Marouf <maroufibrahimi7264@gmail.com>
2023-02-28 16:35:18 -0800

    builder for every product created

---
+++ src/components/Controls/Build/Build.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Build.css";
+
+const Build = () => {
+  return (
+    <div className="builder">
+      <h1>Builder components</h1>
+    </div>
+  );
+};
+
+export default Build;
