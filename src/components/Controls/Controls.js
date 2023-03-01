3086735ffdbed92206aa92828a719dda503264a5
Marouf <maroufibrahimi7264@gmail.com>
2023-02-28 16:40:28 -0800

    Controls component created

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Controls.css";
+import Build from "./Build/Build";
+
+const Controls = () => {
+  return (
+    <div>
+      <Build />
+    </div>
+  );
+};
+
+export default Controls;
