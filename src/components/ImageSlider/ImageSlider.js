ad40712a84c9aa43b10ff4ace0b04a1a8c385f90
Marouf <maroufibrahimi7264@gmail.com>
2023-03-01 21:30:07 -0800

    added slider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-const ImageSlider = ({ images = [], autoPlayTime = 3000 }) => {
+import "./ImageSlider.css";
+
+const ImageSlider = ({ images = [], autoPlayTime = 3000, ...props }) => {
   const [currenSlide, setCurrentSlide] = useState(0);
 
   const nextSlide = (slideIndex = currenSlide + 1) => {
@@ -19,7 +21,14 @@ const ImageSlider = ({ images = [], autoPlayTime = 3000 }) => {
   return (
     <div className="image_slider" {...props}>
       {images.map((item, index) => (
-        <div className="slide" key={index}></div>
+        <div
+          className="slide"
+          key={index}
+          style={{
+            backgroundImage: `url(${item})`,
+            marginLeft: index === 0 ? `-${currenSlide * 100.9}%` : null,
+          }}
+        ></div>
       ))}
     </div>
   );
