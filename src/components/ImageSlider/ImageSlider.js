b366f2a530cd31138354f2aa2e34e9233fcb9e3d
Marouf <maroufibrahimi7264@gmail.com>
2023-03-01 20:57:48 -0800

    maped a div for images

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const ImageSlider = () => {
+const ImageSlider = ({ images = [] }) => {
+  const [currenSlide, setCurrentSlide] = useState(0);
+
   return (
-    <div>ImageSlider</div>
-  )
-}
+    <div className="image_slider" {...props}>
+      {images.map((item, index) => (
+        <div className="slide" key={index}></div>
+      ))}
+    </div>
+  );
+};
 
-export default ImageSlider
+export default ImageSlider;
