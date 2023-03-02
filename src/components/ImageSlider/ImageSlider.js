0b246957390254b3b22d7031ddd27e304b26c26f
Marouf <maroufibrahimi7264@gmail.com>
2023-03-01 21:04:59 -0800

    added function for next image

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const ImageSlider = ({ images = [] }) => {
+const ImageSlider = ({ images = [], autoPlayTime = 3000 }) => {
   const [currenSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = (slideIndex = currenSlide + 1) => {
+    const currenSlideIndex = slideIndex >= images.length ? 0 : slideIndex;
+    setCurrentSlide(currenSlideIndex);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      nextSlide();
+    }, autoPlayTime);
+
+    return () => clearTimeout(timer);
+  }, [currenSlide]);
 
   return (
     <div className="image_slider" {...props}>
