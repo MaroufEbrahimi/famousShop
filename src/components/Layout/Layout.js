7f4be24fe25a2493af6d14d9f6709392abd88187
Marouf <maroufibrahimi7264@gmail.com>
2023-03-01 21:18:20 -0800

    passed images to imageSlider component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 import Wrapper from "../../HOC/Wrapper";
+import ImageSlider from "../ImageSlider/ImageSlider";
+
+import image1 from "../../assets/slideShow_img/slideshow1.jpg";
+import image2 from "../../assets/slideShow_img/slideshow2.jpg";
+import image3 from "../../assets/slideShow_img/slideshow3.jpg";
+import image4 from "../../assets/slideShow_img/slideshow4.jpg";
 
 const Layout = (props) => {
   return (
     <Wrapper>
+      <ImageSlider images={[image1, image2, image3, image4]} />
       <main className="content">{props.children}</main>
     </Wrapper>
   );
