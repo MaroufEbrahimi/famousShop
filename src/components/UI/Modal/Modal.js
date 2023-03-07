5b2a9f4b3ca0d8324695da86f4cdee02b38b2d55
Marouf <maroufibrahimi7264@gmail.com>
2023-03-07 12:34:20 -0800

    calling and passed props to Shopping component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./Modal.css";
 import Wrapper from "../../../HOC/Wrapper";
+import Backdrop from "../Backdrop/Backdrop";
 
 const Modal = (props) => {
   return (
     <Wrapper>
+      <Backdrop show={props.show} removeModal={props.closeModal} />
       <div className="modal">{props.children}</div>
     </Wrapper>
   );
