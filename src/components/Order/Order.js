434f5f4c0a36de6121534be6937071f9c41b2912
Marouf <maroufibrahimi7264@gmail.com>
2023-03-07 13:11:27 -0800

    maped all products

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Order = (props) => {
+  const summery = Object.keys(props.products).map((item) => {
+    return (
+      <li key={item}>
+        {item} : {props.products[item]}
+      </li>
+    );
+  });
+
   return (
     <div>
       <h3>Checkout</h3>
-      <ul></ul>
+      <ul>{summery}</ul>
       <p>Total Price: {props.totalPrice}</p>
       <p>Continue?</p>
     </div>
