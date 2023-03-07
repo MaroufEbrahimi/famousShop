7fa186d19e01645b2c41520b263af6f767726307
Marouf <maroufibrahimi7264@gmail.com>
2023-03-06 22:01:33 -0800

    passing props to another component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Backdrop.css";
 
-const Backdrop = () => {
-  return <div>Backdrop</div>;
+const Backdrop = (props) => {
+  return props.show ? (
+    <div className="backdrop" onClick={props.removeModal}></div>
+  ) : null;
 };
 
 export default Backdrop;
