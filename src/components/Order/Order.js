25f6ecdfc0f083426933eff590e3e20a666e0219
Marouf <maroufibrahimi7264@gmail.com>
2023-03-07 13:06:44 -0800

    ditails for Order com

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Order = () => {
-  return <div>Order</div>;
+const Order = (props) => {
+  return (
+    <div>
+      <h3>Checkout</h3>
+      <ul></ul>
+      <p>Total Price: {props.totalPrice}</p>
+      <p>Continue?</p>
+    </div>
+  );
 };
 
 export default Order;
