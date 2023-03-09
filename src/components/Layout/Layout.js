23eb2df2c21056132cd11474bbd8e88ca2aa2400
Marouf <maroufibrahimi7264@gmail.com>
2023-03-08 20:24:37 -0800

    added NavBar component to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Wrapper from "../../HOC/Wrapper";
 import ImageSlider from "../ImageSlider/ImageSlider";
+import NavBar from "../Navigation/NavBar/NavBar";
 
 import image1 from "../../assets/slideShow_img/slideshow1.jpg";
 import image2 from "../../assets/slideShow_img/slideshow2.jpg";
@@ -11,6 +12,7 @@ import image4 from "../../assets/slideShow_img/slideshow4.jpg";
 const Layout = (props) => {
   return (
     <Wrapper>
+      <NavBar />
       <ImageSlider images={[image1, image2, image3, image4]} />
       <main className="content">{props.children}</main>
     </Wrapper>
