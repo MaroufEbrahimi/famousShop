8bd551a2e8ce544736d10b8ae04867ad0361c05e
marouf ebrahimi <maroufibrahimi7264@gmail.com>
2024-09-25 05:33:55 -0700

    product price fixed

--- src/components/Controls/Product/CartProduct/CartProduct.jsx
+++ src/components/Controls/Product/CartProduct/CartProduct.jsx
@@ -6,7 +6,12 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faSubtract } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const CartProduct = () => {
+const CartProduct = ({
+  productCount,
+  increaseProduct,
+  decreaseProduct,
+  productPrice,
+}) => {
   return (
     <div className="cart_product_list display_flex align_items_center justify_content_space_between">
       <div className="cart_product_info display_flex align_items_center gap_20">
@@ -14,17 +19,29 @@ const CartProduct = () => {
         <span>prodcut meessage should be here</span>
       </div>
       <div className="cart_product_inc_dec display_flex align_items_center gap_10">
-        <FontAwesomeIcon icon={faPlus} className="fa_icon" />
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="fa_icon"
+          onClick={increaseProduct}
+        />
         <div className="cart_pro_inc_box display_flex align_items_center justify_content_center">
-          1
+          {productCount}
         </div>
-        <FontAwesomeIcon icon={faSubtract} className="fa_icon" />
-        <FontAwesomeIcon icon={faTrash} className="fa_icon" />
+        {productCount > 1 ? (
+          <FontAwesomeIcon
+            icon={faSubtract}
+            className="fa_icon"
+            onClick={decreaseProduct}
+          />
+        ) : (
+          <FontAwesomeIcon icon={faTrash} className="fa_icon red_color" />
+        )}
       </div>
       <div className="cart_product_amount">
         <p>Total amount</p>
         <p className="bold">
-          <sup>$ </sup>3,124
+          <sup>$ </sup>
+          {productPrice}
         </p>
       </div>
     </div>
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cart.css";
 import CartProduct from "../../components/Controls/Product/CartProduct/CartProduct";
 
 const Cart = () => {
+  const [productCount, setProductCount] = useState(1);
+  const unitPrice = 290;
+  const [productPrice, setProductPrice] = useState(unitPrice);
+
+  const increaseProduct = () => {
+    setProductCount((p) => p + 1);
+    setProductPrice((productCount + 1) * unitPrice);
+  };
+  const decreaseProduct = () => {
+    setProductCount((p) => (p > 0 ? p - 1 : 0));
+    setProductPrice(
+      productCount > 1 ? (productCount - 1) * unitPrice : unitPrice
+    );
+  };
+
+  // format the price bigger than 1000
+  const formattedPrice =
+    productPrice >= 1000 ? productPrice.toLocaleString() : productPrice;
+
   return (
     <div className="cart display_flex gap_40 dummy_margin">
       <div className="cart_list">
-        <CartProduct />
-        <CartProduct />
-        <CartProduct />
-        <CartProduct />
+        <CartProduct
+          productCount={productCount}
+          increaseProduct={() => increaseProduct()}
+          decreaseProduct={() => decreaseProduct()}
+          productPrice={formattedPrice}
+        />
       </div>
       <div className="cart_summary">1</div>
     </div>
